a0133bd94cbbc774b981d6b3a26cf23ed6e994bc
yui-Kitamura <yui@yui-kitamura.eng.pro>
2026-07-21 20:48:48 +0900

    JSON output lib

--- src/main/java/pro/eng/yui/android/osmjppostalmapdatasource/Main.java
+++ src/main/java/pro/eng/yui/android/osmjppostalmapdatasource/Main.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.HashMap;
