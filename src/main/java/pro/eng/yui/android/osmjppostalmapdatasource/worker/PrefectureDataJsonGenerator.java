b0340301998f31a1fd31a97442db8b6a5f6f39de
yui-Kitamura <yui@yui-kitamura.eng.pro>
2026-07-21 21:43:57 +0900

    都道府県ごとのデータファイルの殻作成

---
+++ src/main/java/pro/eng/yui/android/osmjppostalmapdatasource/worker/PrefectureDataJsonGenerator.java
@@ -0,0 +1,52 @@
+package pro.eng.yui.android.osmjppostalmapdatasource.worker;
+
+import pro.eng.yui.android.osmjppostalmapdatasource.Main;
+import pro.eng.yui.oss.osm.lib.jppostalcore.types.OsmPoi;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PrefectureDataJsonGenerator {
+
+    public PrefectureDataJsonGenerator() {
+
+    }
+    
+    public static class Result{
+        private final int code;
+        public int getPrefCode(){ return code; }
+        private final LocalDate dataTimestamp;
+        public LocalDate getDataTimestamp(){ return dataTimestamp; }
+        private final Map<String, Object> data;
+        public Map<String, Object> getJsonData(){ return data; }
+        
+        public Result(int prefCode, LocalDate timestamp, Map<String, Object> jsonData){
+            this.code = prefCode;
+            this.dataTimestamp = timestamp;
+            this.data = jsonData;
+        }
+    }
+
+    /** 
+     * @param prefCode 都道府県コード
+     * @param name 都道府県名 */
+    public Result generate(int prefCode, String name) {
+        Map<String, Object> data = new HashMap<>();
+
+        List<OsmPoi> pois = new ArrayList<>();
+        //FIXME 
+        
+        LocalDate timestamp = LocalDate.now(Main.JST);
+        data.put("lastModified", timestamp);
+        data.put("prefectureCode", prefCode);
+        data.put("name", name);
+        data.put("data", pois);
+        
+        return new Result(prefCode, timestamp, data);
+    }
+}
+
